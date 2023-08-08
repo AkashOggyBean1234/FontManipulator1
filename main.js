20437e542d6f51c5032a75c2a96e1a5def3565cb
AkashOggyBean1234 <116659557+AkashOggyBean1234@users.noreply.github.com>
2023-08-08 20:28:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw()
+{
+    background("#47f5f5");
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet Is Initialised!");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
